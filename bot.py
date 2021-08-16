599cddd1e39fb4b0120adc9147d43e2b5804d1f4
Jeremy Brezenoff <jeremybrezenoff@gmail.com>
2021-08-16 17:08:29 -0400

    Installed plotly

--- bot.py
+++ bot.py
@@ -12,9 +12,11 @@ KEY = os.getenv('POLYTON_KEY')
 
 bot = commands.Bot(command_prefix='./')
 
+
 @bot.command(name='echo', help='Repeats the argument back to you')
 async def command_echo(ctx, arg):
     await ctx.send(arg)
+
 
 @bot.command(name='view', help='<symbol> View the stock represented by <symbol>')
 async def command_view(ctx, symbol):
@@ -24,9 +26,10 @@ async def command_view(ctx, symbol):
     message = f'Yesterday for {symbol}:\nOpen: ${r["o"]}\nClose: ${r["c"]}\nHigh: ${r["h"]}\nLow: ${r["l"]}'
     await ctx.send(message)
 
+
 def main():
     bot.run(TOKEN)
 
+
 if __name__ == '__main__':
     main()
-
