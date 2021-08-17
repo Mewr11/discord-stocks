34491797f0cdadf879e1774df49075a439a5903b
Isaac Galang <isaac.t.galang@gmail.com>
2021-08-17 13:51:23 -0500

    Added add and remove commands for the daily report

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='./')
+guild_report = {}
 
 
 @bot.command(name='echo', help='Repeats the argument back to you')
@@ -17,7 +18,7 @@ async def command_echo(ctx, arg):
     await ctx.send(arg)
 
 
-@bot.command(name='view', help='<symbol> View the stock represented by <symbol>')
+@bot.command(name='view', help='<symbol> View the current, open, and daily high and low of a stock')
 async def command_view(ctx, symbol):
     ticker = yf.Ticker(symbol)
     hist = ticker.history(period="1d")
@@ -25,6 +26,27 @@ async def command_view(ctx, symbol):
     d = hist.index[0].date().strftime('%d %b %Y')
     await ctx.send(f'{d} for {symbol.upper()}:\nCurrent - ${c:.2f}\nOpen - ${o:.2f}\nHigh - ${h:.2f}\nLow - ${l:.2f}')
 
+@bot.command(name='add', help='<symbol> Add a stock to the daily report')
+async def command_add(ctx, symbol):
+    guild_id = ctx.guild.id
+    if guild_id in guild_report:
+        guild_report[guild_id].append(symbol.upper())
+    else:
+        guild_report[guild_id] = [symbol.upper()]
+    await ctx.send('Current Report:\n' + ' - '.join(guild_report[guild_id]))
+
+@bot.command(name='remove', help='<symbol> Remove a stock from the daily report')
+async def command_remove(ctx, symbol):
+    guild_id = ctx.guild.id
+    symbol = symbol.upper()
+    if guild_id in guild_report:
+        if symbol in guild_report[guild_id]:
+            guild_report[guild_id].remove(symbol)
+            await ctx.send(f'Removed {symbol}. Current Report:\n' + ' - '.join(guild_report[guild_id]))
+        else:
+            await ctx.send(f'{symbol} is not in the current report.')
+    else:
+        await ctx.send(f'{symbol} is not in the current report.')
 
 def main():
     bot.run(TOKEN)
